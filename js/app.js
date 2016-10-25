045644aa494b638a17d7307bbdcb22bfbc045cdc
aifos710 <sofia.m.caballero@gmail.com>
2016-10-24 19:55:33 -0500

    html2

--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
 $(document).ready(function(){
 
+	//lenguajes
 	$(document).ready(function() {
     $('select').material_select();
   });
+
+	// 
+	$('ul.tabs').tabs();
    
    /* $('.dropdown-button').dropdown('open');
     $('.dropdown-button').dropdown('close');*/
